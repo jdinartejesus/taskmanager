a045eb68c141d02e86f4658349645dee1cb54af9
Dinarte Jesus <jdinartejesus@gmail.com>
2016-02-21 02:53:37 +0100

    Add development envoriment

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# taskmanager
+# Task Manager [![wercker status](https://app.wercker.com/status/704e851b57524241ac8610995a396328/s "wercker status")](https://app.wercker.com/project/bykey/704e851b57524241ac8610995a396328)
 Small app to manage time and tasks using pure javascript (ES6) with purpose of learning the language.
---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+const gulp  = require('gulp');
+const babel  = require('gulp-babel');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const eslint = require('gulp-eslint');
+const inject = require('gulp-inject');
+const jade = require('gulp-jade');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const stylelint = require('stylelint');
+const autoprefixer = require('autoprefixer');
+const minify = require('cssnano');
+
+const paths = {
+  scripts: 'src/assets/js/app.js',
+  styles: 'src/assets/styles/*.scss',
+  jades: 'src/**/*.jade'
+};
+
+// Compile the javascript
+gulp.task('scripts', () => {
+  gulp.src(paths.scripts)
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(concat('app.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('build'));
+});
+
+// Compile sass into css
+gulp.task('styles', () => {
+  gulp.src(paths.styles)
+    .pipe(sass())
+    .pipe(postcss([autoprefixer, stylelint, minify]))
+    .pipe(gulp.dest('build'));
+});
+
+// Compile jades to html
+gulp.task('jades', () => {
+  gulp.src(paths.jades)
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(inject(gulp.src(['./dist/**/*.js', './dist/**/*.css'], {read: false})))
+    .pipe(gulp.dest('build'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('src/**/*.js', 'scripts');
+    gulp.watch('src/**/*.scss', 'styles');
+    gulp.watch('stc/**/*.jade', 'jades');
+});
+
+gulp.task('default', ['scripts', 'styles', 'jades']);

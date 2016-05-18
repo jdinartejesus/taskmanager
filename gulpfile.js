54b60867fc8cb89f0bacc268978c34b18d4ad604
Dinarte Jesus <jdinartejesus@gmail.com>
2016-05-18 20:00:40 +0200

    Updated build adding gulp-connect and task buikd

--- gulpfile.js
+++ gulpfile.js
@@ -9,25 +9,27 @@ const eslint = require('gulp-eslint');
 const inject = require('gulp-inject');
 const jade = require('gulp-jade');
 const sass = require('gulp-sass');
+const connect = require('gulp-connect')
 const postcss = require('gulp-postcss');
 const stylelint = require('stylelint');
 const autoprefixer = require('autoprefixer');
 const minify = require('cssnano');
+const browserify = require('browserify');
 
 const paths = {
   scripts: 'src/assets/js/app.js',
-  styles: 'src/assets/styles/*.scss',
-  jades: 'src/**/*.jade',
+  styles: 'src/assets/styles/default.scss',
+  jades: 'src/index.jade',
 };
 
 // Compile the javascript
 gulp.task('scripts', function() {
   gulp.src(paths.scripts)
+    .pipe(eslint())
+    .pipe(eslint.format())
     .pipe(babel({
       presets: ['es2015'],
     }))
-    .pipe(eslint())
-    .pipe(eslint.format())
     .pipe(concat('app.js'))
     .pipe(uglify())
     .pipe(gulp.dest('build/js'));
@@ -47,15 +49,26 @@ gulp.task('jades', function() {
     .pipe(jade({
       pretty: true,
     }))
-    .pipe(inject(gulp.src(['./build/**/*.js', './build/**/*.css'], { read: false })))
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('build'))
+    .pipe(inject(gulp.src(['./js/app.js', './styles/default.css'], { read: false })));
+});
+
+// Server to run application
+gulp.task('connect', function() {
+  connect.server({
+    root: 'build',
+    port: 8000,
+    livereload: true
+  });
 });
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-  gulp.watch('src/**/*.js', 'scripts');
-  gulp.watch('src/**/*.scss', 'styles');
-  gulp.watch('stc/**/*.jade', 'jades');
+  gulp.watch('src/**/*.js', ['scripts']);
+  gulp.watch('src/**/*.scss', ['styles']);
+  gulp.watch('src/**/*.jade', ['jades']);
 });
+
+gulp.task('build', ['scripts', 'styles', 'jades', 'connect', 'watch']);
 
 gulp.task('default', ['scripts', 'styles', 'jades']);
